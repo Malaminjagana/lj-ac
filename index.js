432e4aa4166f3617a775f70fac4d2333508c8106
Malamin Jagana <jaganalamin@gmail.com>
2024-12-15 04:08:45 +0100

    first js p

---
+++ index.js
@@ -0,0 +1,11 @@
+let fullname = "malmainjagana";
+let sport = 1 * 3;
+//et totalPrice = 50; // from the task above
+//let discountedPrice = totalPrice * 0.9;
+// let budget = 40;
+// let canPay = discountedPrice >= budget;
+let totalPrice = 50;// from the task above
+let discountedPrice = totalPrice * 0.9;
+let budget = 40; 
+let canpay = discountedPrice >= budget;
+//traionin
